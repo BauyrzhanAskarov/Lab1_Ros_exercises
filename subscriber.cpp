c5317c6ba874a918f7aa10bb2566d33aa0798e4a
BauyrzhanAskarov <79759752+BauyrzhanAskarov@users.noreply.github.com>
2024-09-03 04:29:37 +0500

    Add files via upload

--- subscriber.cpp
+++ subscriber.cpp
@@ -1,58 +1,104 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Spawn.h"
+#include "turtlesim/TeleportAbsolute.h"
+#include "turtlesim/Kill.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
+//ros::Publisher pub;
+ros::Publisher velocity_pub;
+
+//void turtleCallback(const turtlesim::Pose::ConstPtr& msg)
+//{
+	//ROS_INFO("Turtle subscriber@[%f, %f, %f]",
+	//msg->x, msg->y, msg->theta);
+	//geometry_msgs::Twist my_vel;
+	//my_vel.linear.x = 1.0;
+	//my_vel.angular.z = 1.0;
+	// pub.publish(my_vel); 
+//}
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
+    // Initialize the node, setup the NodeHandle for handling the communication with the ROS system
+    ros::init(argc, argv, "turtlebot_subscriber");
+    ros::NodeHandle nh;
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+    // Kill the default turtle
+    ros::ServiceClient kill_client = nh.serviceClient<turtlesim::Kill>("/kill");
+    turtlesim::Kill kill_srv;
+    kill_srv.request.name = "turtle1"; 
+    kill_client.call(kill_srv);
+    
+    // Spawn a new turtle at the center
+    ros::ServiceClient spawn_client = nh.serviceClient<turtlesim::Spawn>("/spawn");
+    turtlesim::Spawn spawn_srv;
+    spawn_srv.request.x = 5.5;
+    spawn_srv.request.y = 5.5;
+    spawn_srv.request.theta = 0.0;
+    spawn_srv.request.name = "turtle_Bauyrzhan";
+    spawn_client.call(spawn_srv);
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+    // Teleport to the first corner
+    ros::ServiceClient teleport_client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle_Bauyrzhan/teleport_absolute");
+    turtlesim::TeleportAbsolute teleport_srv;
+    teleport_srv.request.x = 4.5;
+    teleport_srv.request.y = 4.5;
+    teleport_srv.request.theta = 0.0;
+    teleport_client.call(teleport_srv);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+    // Initialize publisher for the turtle velocity
+    velocity_pub = nh.advertise<geometry_msgs::Twist>("/turtle_Bauyrzhan/cmd_vel", 10);
+    geometry_msgs::Twist vel_msg;
+    ros::Rate rate(1);
 
-  return 0;
+    // Square movement
+    for (int i = 0; i < 4; ++i)
+    {
+    	// Turn 90 degrees
+        vel_msg.linear.x = 0.0;
+        vel_msg.angular.z = -1.57;  // -90 degrees in radians
+        velocity_pub.publish(vel_msg);
+        ros::Duration(1.0).sleep(); // Turn for 1 second 
+        
+        // Move forward
+        vel_msg.linear.x = 2.0;
+        vel_msg.angular.z = 0.0;
+        velocity_pub.publish(vel_msg);
+        ros::Duration(2.0).sleep(); // Move for 2 seconds
+
+    }
+
+    // Triangular movement
+    for (int i = 0; i < 3; ++i)
+    {	
+    	
+	if (i != 2) {
+		// Turn 90 degrees
+		vel_msg.linear.x = 0.0;
+		vel_msg.angular.z = -1.57;  // -90 degrees in radians
+		velocity_pub.publish(vel_msg);
+		ros::Duration(1.0).sleep(); // Turn for 1 second
+		// Move forward
+        	vel_msg.linear.x = 2.0;
+        	vel_msg.angular.z = 0.0;
+        	velocity_pub.publish(vel_msg);
+        	ros::Duration(2.0).sleep(); // Move for 2 seconds
+		
+	} else {
+		vel_msg.linear.x = 0.0;
+		vel_msg.angular.z = -1.57/2-1.57;  // -135 degrees in radians
+		velocity_pub.publish(vel_msg);
+		ros::Duration(1.0).sleep(); // Turn for 1 second
+		
+		vel_msg.linear.x = 2.0*1.41;
+        	vel_msg.angular.z = 0.0;
+        	velocity_pub.publish(vel_msg);
+        	ros::Duration(2.0).sleep(); // Move for 2 seconds
+		
+	}
+        
+    }
+
+    return 0;
 }
